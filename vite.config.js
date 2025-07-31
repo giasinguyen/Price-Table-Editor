8c31e475ce5d98649d584dded073b592f8110acf
giasinguyen <giasinguyentran@gmail.com>
2025-07-31 17:24:09 +0700

    Remove GitHub Pages deployment workflow and update homepage in package files

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Price-Table-Editor/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
